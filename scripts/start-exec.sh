bd5f601127d5f3d8eabb6983439c956ea319a0a7
Harun Ulum Fajar <harun.ulum@ruangguru.com>
2024-09-02 16:20:38 +0700

    [azkaban] fix: handle azk change mysql target env to pxc

--- scripts/start-exec.sh
+++ scripts/start-exec.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+get_env="${1:-staging}"
+
+# check if the environment is valid
+if [ "$get_env" != "staging" ] || [ "$get_env" != "production" ]; then
+  echo "invalid environment!"
+  exit 1
+fi
+
 cp /secrets/azkaban-properties/azkaban.properties /azkaban/conf
 cp /secrets/azkaban-users-xml/azkaban-users.xml /azkaban/conf
-/scripts/wait_for_port_ready.py 3306 15
+/scripts/wait_for_port_ready.py 3306 15 $get_env
 /azkaban/bin/start-exec.sh
-/scripts/wait_for_port_ready.py 12321 15
+/scripts/wait_for_port_ready.py 12321 15 $get_env
 /scripts/executor_action.py activate
 /scripts/reload_exec.py
 tail -f /azkaban/executorServerLog_*
--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+get_env="${1:-staging}"
+
+# check if the environment is valid
+if [ "$get_env" != "staging" ] || [ "$get_env" != "production" ]; then
+  echo "invalid environment!"
+  exit 1
+fi
+
 cp /secrets/azkaban-properties/azkaban.properties /azkaban/conf
 cp /secrets/azkaban-users-xml/azkaban-users.xml /azkaban/conf
 /scripts/wait_for_port_ready.py 3306 30
