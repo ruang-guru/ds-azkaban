f2e3e0f01eacfc4e2d3385243c0b1d09654f3be5
Harun Ulum Fajar <harun.ulum@ruangguru.com>
2024-09-02 15:49:51 +0700

    [cloudsql] fix: enable get_env init from params bash

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+get_env="${1:-staging}"
+
+if [ "$get_env" != "staging" ] || [ "$get_env" != "production" ]; then
+  echo "invalid environment!"
+  exit 1
+fi
+
 cp /secrets/azkaban-properties/azkaban.properties /azkaban/conf
 cp /secrets/azkaban-users-xml/azkaban-users.xml /azkaban/conf
-/scripts/wait_for_port_ready.py 3306 30
+/scripts/wait_for_port_ready.py 3306 30 $get_env
 /azkaban/bin/start-web.sh
-/scripts/wait_for_port_ready.py 8081 45
+/scripts/wait_for_port_ready.py 8081 45 $get_env
 /scripts/reload_exec.py local
 tail -f /azkaban/webServerLog_*
